e79ab18a3c14ce8bdb1b529e49a3b569fce43442
vernonthedev <techjaja2@gmail.com>
2025-12-01 11:44:25 +0300

    chore: added automated semantic versioning, fixes #3

---
+++ release.config.js
@@ -0,0 +1,23 @@
+export default {
+  branches: ["main"],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/changelog",
+      {
+        changelogFile: "CHANGELOG.md",
+        changelogTitle: "# Changelog",
+      },
+    ],
+    "@semantic-release/github",
+    [
+      "@semantic-release/git",
+      {
+        assets: ["CHANGELOG.md", "package.json", "yarn.lock"],
+        message:
+          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
+  ],
+};
